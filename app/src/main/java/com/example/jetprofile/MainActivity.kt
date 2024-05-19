7e1faec743a355da932360d07924c9c85b2b1eec
gitlearner246 <yourverygoodfriendaki@gmail.com>
2024-05-19 10:14:33 +0900

    Company Name

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.example.jetprofile
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +38,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(20.dp)) {
+                        // Profile pic
+                        Image(painter = painterResource(id = R.drawable.img_profile),
+                            contentDescription = "Profile picture",
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(10.dp)))
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
                         // Name
                         Text(
                             text = "ABC DEFG",
@@ -46,6 +64,23 @@ class MainActivity : ComponentActivity() {
                             color = Color.Gray,
                             fontSize = 16.sp,
                         )
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Column(horizontalAlignment = Alignment.Start,
+                            modifier = Modifier.fillMaxWidth(),) {
+                            // Employer
+                            Text(
+                                text = "BINGGGG",
+                                fontSize = 26.sp,
+                                fontWeight = FontWeight.Bold,
+                           )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            // Department
+                            Text(text = "GTPGTP",
+                                color = Color.Gray,
+                                fontSize = 16.sp)
+
+                        }
                     }
                 }
             }
